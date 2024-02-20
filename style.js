ba6a8396771b769c6a810b87969e42abe009ebed
Aaronerar <aaronnader@hotmail.com>
2024-02-20 19:27:42 -0300

    Projeto Rak

---
+++ style.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const menuIcon = document.querySelector('.menu-icon');
+    const nav = document.querySelector('.nav');
+    menuIcon.addEventListener('click', function() {
+        nav.classList.toggle('menu-open');
+    });
+});
